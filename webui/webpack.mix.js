1f3d46dfd6cfbc9219851a23f5c5a1e452d0c21a
Tristan Seifert <tristan@tseifert.me>
2022-09-13 20:34:23 -0700

    Add web fonts

--- webui/webpack.mix.js
+++ webui/webpack.mix.js
@@ -13,6 +13,31 @@ const mix = require('laravel-mix');
 
 mix.js('resources/js/app.js', 'public/js').extract()
    .sass('resources/css/app.scss', 'public/css')
+    .copy('resources/fonts/**/*.woff', 'public/fonts')
+    .copy('resources/fonts/**/*.woff2', 'public/fonts')
+
+.webpackConfig({
+    module: {
+        rules: [
+            // We're registering the TypeScript loader here. It should only
+            // apply when we're dealing with a `.ts` or `.tsx` file.
+            {
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                options: { appendTsSuffixTo: [/\.vue$/] },
+                exclude: /node_modules/,
+            },
+        ],
+    },
+
+    resolve: {
+        // We need to register the `.ts` extension so Webpack can resolve
+        // TypeScript modules without explicitly providing an extension.
+        // The other extensions in this list are identical to the Mix
+        // defaults.
+        extensions: ['*', '.js', '.jsx', '.vue', '.ts', '.tsx'],
+    },
+});
 
 // version the CSS/JS in prod
 if(mix.inProduction()) {
